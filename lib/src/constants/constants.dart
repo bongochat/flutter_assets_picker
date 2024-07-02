a256b6880e859a08711991472022f1660884eade
sajib1066 <sajib1066@gmail.com>
2024-07-03 00:32:47 +0800

    first commit

---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,13 @@
+// Copyright 2019 The bongochat author. All rights reserved.
+// Use of this source code is governed by an Apache license that can be found
+// in the LICENSE file.
+
+import 'package:photo_manager/photo_manager.dart';
+
+const String packageName = 'bongochat_image_picker';
+
+const int defaultAssetsPerPage = 80;
+const int defaultMaxAssetsCount = 9;
+
+const ThumbnailSize defaultAssetGridPreviewSize = ThumbnailSize.square(200);
+const ThumbnailSize defaultPathThumbnailSize = ThumbnailSize.square(80);
